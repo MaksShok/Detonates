9b14604f3e692419d2fb19e02f29284bd5263eeb
SkreT1kk <simanovdaniil44@gmail.com>
2026-02-26 14:13:26 +0300

    PlayerController and CameraFollow

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5f;
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        rb.gravityScale = 0f;        
+        rb.freezeRotation = true;    
+        rb.linearDamping = 10f;      
+    }
+    
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        
+        Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+        
+        rb.linearVelocity = moveDirection * Speed;
+    }
+}
